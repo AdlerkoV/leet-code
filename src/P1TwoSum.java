a27d9b5cb00516f31aabcb34f342cac49b2440ab
Minh Ngo Le ( 2533 ) <minhngole@c02g51wmml7h.lan>
2023-06-05 17:00:17 +0700

    Initial commit

---
+++ src/P1TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class P1TwoSum {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Problem 1");
+        int[] a = new int[] { 1, 2, 3 };
+        System.out.println(Arrays.toString(twoSum(a, 3)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+}
---
+++ src/P21MergeTwoSortedLists.java
@@ -0,0 +1,31 @@
+public class P21MergeTwoSortedLists {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Problem 21");
+
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
